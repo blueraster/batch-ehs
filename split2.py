68f0125cf1e420277c11409efcd1704ea0e58a91
Chris Gabris <cjgabris@gmail.com>
2016-04-01 16:00:54 -0400

    split shapefile into multiple based on UID

---
+++ split2.py
@@ -0,0 +1,30 @@
+
+import arcpy
+import os
+import time
+
+starttime = time.time()
+
+inputFile = r'P:\Data\global_admin_boundaries\Admin0.shp'
+outDir = r'P:\2278 World Resources Institute\GFW\emerging hotspots\Publication\HansenLoss\6_shapefiles\Admin0'
+
+rows = arcpy.SearchCursor(inputFile)
+row = rows.next()
+attribute_types = set([])
+
+while row:
+    attribute_types.add(row.UID)
+    row = rows.next()
+
+
+for each_attribute in attribute_types:
+    # outSHP = outDir + "_"+ each_attribute + r'.shp'
+    outSHP = os.path.join(outDir,each_attribute+r".shp")
+    print outSHP
+    where = "\"UID\" = '" + each_attribute + "'"
+    arcpy.Select_analysis (inputFile, outSHP, where)
+    
+
+del rows, row, attribute_types
+print "script complete!"
+print (time.time()-starttime)
